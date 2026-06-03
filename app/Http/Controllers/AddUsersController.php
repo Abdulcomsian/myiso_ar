4bb4efddc988405ff829a8eb713d58afb7b337b5
Abdul Basit <basitawan.abdul@gmail.com>
2026-06-03 17:57:52 +0500

    admin side login history user name and logo

--- app/Http/Controllers/AddUsersController.php
+++ app/Http/Controllers/AddUsersController.php
@@ -124,8 +124,25 @@ class AddUsersController extends Controller
         
         $loginHistory = LoginHistoryUser::where('user_id', $user_id)->orderBy('id', 'desc')->get();
         // dd($loginHistory);
+
+        $user = AddUsers::where('id', $user_id)->first();
+
+        $userLogo = '';
+        $username = '';
+        if (isset($user->profile_image)) {
+            $userLogo = 'https://myisoonline.com/' . $user->profile_image;
+            $username = $user->name ?? '';
+        }
         
-        $list = '<table class="table">
+        $list = '<div class="d-flex gap-1 mb-4 align-items-center" style="gap: 5px;">
+            <div>
+                <img src="' . $userLogo . '" class="rounded-circle" style="width:50px;height:50px" />
+            </div>
+            <div class="flex-grow-1 ms-2">
+                <h5 class="mb-1">' . $username . '</h5>
+            </div>
+        </div>
+        <table class="table">
         <thead>
             <tr>
                 <th>الرقم</th>
