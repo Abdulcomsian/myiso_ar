3a018acd423df699ca9a0d0f46ba60012437008e
Abdul Basit <basitawan.abdul@gmail.com>
2026-06-03 16:12:05 +0500

    login terms and conditions in arabic

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -102,126 +102,58 @@
 										<div class="modal-dialog modal-dialog-scrollable">
 											<div class="modal-content">
 											<div class="modal-header">
-												<h5 class="modal-title" id="exampleModalLabel"> TERMS & CONDITIONS</h5>
-												<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-												<span aria-hidden="true">&times;</span>
-												</button>
+												<h5 class="modal-title" id="exampleModalLabel" style="text-align: right">الشروط والأحكام الخاصة بشركة </h5>
+												<a data-dismiss="modal" aria-label="Close">
+													<i class="fa fa-times" aria-hidden="true"></i>
+												</a>
 											</div>
-											<div class="modal-body">
-												<h6>Agreement to Terms</h6>
-												<p>This document governs your relationship with  MYISOONLINE™.<br>  ISOONLINE E SOLUTIONS LLC SPC owns
-													and   operates   this   website.   Access   to   and   use   of   this   website   and   the
-													products   and   services   available   through   this   website   (collectively,   the
-													"Services") are subject to the following terms, conditions, and notices (the
-													"Terms   of   Service")   By   checking   the   "Terms   and   Conditions,"   you
-													acknowledge that you understand and are agreeable with these Terms and
-													Conditions. By using the Services, you agree to all the Terms of Service, as
-													may   be   updated   by   us   from   time   to   time.   You   should   check   this   page
-													regularly to take notice of any changes we may have made to the Terms of
-													Service. We will not be liable if, for any reason, this Website is unavailable at
-													any time or for any period. From time to time, we may restrict access to
-													some parts or all this Website.</p><br>
-												<h6>Prohibitions</h6>
-												<p>You   must   not   misuse   this   Website.   You   will   not:   commit   or   encourage   a
-													criminal offense; transmit or distribute a virus or any other material which is
-													malicious, technologically harmful, in breach of confidence, or in any way
-													offensive or obscene. Breaching this provision  would  constitute  a criminal
-													offense,   and  ISOONLINE E SOLUTIONS LLC SPC   will   report   any   such   breach   to   the   relevant   law
-													enforcement authorities and disclose your identity to them.</p><br>
-													<h6>Intellectual Property, Software, and Content</h6>
-													<p>The   intellectual   property   rights   in   all   software   and   content   (including
-													photographic   images)   made   available   to   you   on   or   through   this   Website
-													remains the property of J M Enterprises and are protected by copyright laws and
-													treaties worldwide. You may store, print, and display the content supplied
-													solely   for   personal   use.   You   are   not   permitted   to   publish,   manipulate,
-													distribute,   or   otherwise   reproduce,   in   any   format,   any   of   the   content   or
-													copies of the content supplied to you or which appears on this Website, nor
-													may   you   use   any   such   content   in   connection   with   any   business   or
-													commercial enterprise.</p><br>
-													<h6>Terms of Service</h6>
-													<p>We collaborate with outside Independent Auditors and Certification Bodies
-													who can provide to users of Myisoonline™ ISO certification upon completion
-													of a successful audit.  Myisoonline™ cannot award certificates to users as is
-													not an Accreditation Body nor Certification Body. Each Myisoonline™ software license is valid for three years, subject to three
-													successful annual audits. After purchasing our system and should you wish
-													to gain ISO Certification, you must continue to use it and demonstrate usage
-													by passing each annual audit; otherwise, your Certificate will be revoked. If
-													your Certificate is revoked, Myisoonline will not be held responsible.</p><br>
-
-													<h6>Returns</h6>
-													<p>Since we deal in online certification and it's a digital service, we only offer a
-													refund within 7 days during service purchase. We request you to check out
-													the  demo video,   our  free  blogs,  or  chat  with our  agent, available  on  our
-													Website.</p><br>
-													<h6>Refunds (if applicable)</h6>
-													<p>We don't accept refund claims. Due to the nature of digital products and
-													services, we cannot honour refund claims after seven days.</p><br>
-
-													<h6>Disclaimer of Liability</h6>
-													<p>The material displayed on this Website is provided without any guarantees,
-													conditions, or warranties regarding its accuracy. Unless expressly stated to
-													the contrary to the fullest extent permitted by law J M Enterprises and its suppliers,
-													content providers, and advertisers hereby expressly exclude all conditions,
-													warranties, and other terms which might otherwise be implied by statute,
-													common law, or the law of equity and shall not be liable for any damages
-													whatsoever, including but without limitation to any direct, indirect, special,
-													consequential, punitive or incidental damages, or damages for loss of use,
-													profits, data or other intangibles, damage to goodwill or reputation, or the
-													cost   of   procurement   of   substitute   goods   and   services,   arising   out   of   or
-													related to the use, inability to use, performance or failures of this Website or
-													the Linked Sites and any materials posted thereon, irrespective of whether
-													such damages were foreseeable or arise in contract, tort, equity, restitution,
-													by statute, at common law or otherwise.</p><br>
-
-													<h6>Disclaimer as to ownership of trademarks, images of personalities,
-													and third-party copyrigh</h6>
-													<p>Except where expressly stated to the contrary, all persons (including their
-													names   and   images),   third-party   trademarks   and   content,   services,   and
-													locations   featured   on   this   Website   are   in   no   way   associated,   linked,   or
-													affiliated with J M Enterprises, and you should not rely on the existence of such a
-													connection   or   affiliation.   The   respective   trademark   owners   own   any trademarks/names featured on this Website. Where a trademark or brand
-													name is referred to, it is used solely to describe or identify the products and
-													services and is in no way an assertion that such products or services are
-													endorsed by or connected to J M Enterprises. </p><br>
-
-													<h6>Indemnity</h6>
-													<p>You agree to indemnify, defend and hold harmless J M Enterprises, its directors,
-													officers, employees, consultants, agents, and affiliates, from any third-party
-													claims, liability, damages, and costs (including, but not limited to, legal fees)
-													arising from your use this Website or your breach of the Terms of Service.</p><br>
-													<h6>Variation</h6>
-													<p>J M Enterprises shall have the right in its absolute discretion at any time and without
-													notice to amend, remove or vary the Services and any page of this Website.</p><br>
-													<h6>Invalidity</h6>
-													<p>If any part of the Terms of Service is unenforceable (including any provision
-													in which we exclude our liability to you), the enforceability of any other part
-													of the Terms of Service will not be affected by all other clauses remaining in
-													full force and effect. So far as possible, where any clause/sub-clause or part
-													of a clause/sub-clause can be severed to render the remaining part valid, the
-													clause   shall   be   interpreted   accordingly.   Alternatively,   you   agree   that   the
-													clause shall be rectified and interpreted in a way that closely resembles the
-													original meaning of the clause /sub-clause as permitted by law.</p><br>
-													<h6>Governing Law/Jurisdiction</h6>
-													<p>These terms and conditions and the transactions contemplated hereby shall
-													be governed by, and construed and interpreted under, the laws of the United
-													Arab Emirates. Any action seeking legal or equitable relief arising out of or
-													relating to these Terms will be brought only in the courts of the United Arab
-													Emirates.</p><br>
-													<h6>Entire Agreement</h6>
-													<p>The  above  Terms  of  Service  constitute   the  parties'   entire  agreement and
-													supersede  any preceding and contemporaneous  agreements between you
-													and J M Enterprises. Any waiver of any provision of the Terms of Service will be
-													effective only if in writing and signed by a Director of J M Enterprises.</p><br>
-													<h6>CONTACT US</h6>
-													<p>If you have questions or comments about this Terms and Conditions, please
-													get in touch with us at: <br>
-													ISOONLINE E SOLUTIONS LLC SPC<br>
-													Office 1003, C34 Building,<br>
-													Ar Razqi St, Al Danah,<br>
-													Abu Dhabi, United Arab Emirates, 22207<br>
-													Email: ceo@jmegrp.com 
+											<div class="modal-body" style="text-align: right; direction: rtl">
+												<h6><strong>الموافقة على الشروط</strong></h6>
+												<p>تنظم هذه الوثيقة العلاقة بينك وبين موقع "ماي آيزو أونلاين" (MYISOARABIA) الإلكتروني. تملُك شركة ج. م. إنتربرايزس ذ.م.م المنطقة الحرة هذا الموقع الإلكتروني وتعمل على تشغيله وإدارته. إن وصولك إلى هذا الموقع واستخدامه، بالإضافة إلى المنتجات والخدمات المقدمة من خلاله (يشار إليها مجتمعة باسم "الخدمات")، تحكمه الشروط والأحكام والإشعارات اللاحقة، والتي يشار إليها باسم ("شروط الخدمة"). من خلال تأكيد موافقتك على "الشروط والأحكام"، أنت تقرّ بأنك تفهم هذه الشروط والأحكام وتوافق على الالتزام بها. وباستخدامك لهذه الخدمات، فإنك توافق بموجب هذا على الالتزام بجميع شروط الخدمة، والتي قد نقوم بمراجعتها وتحديثها بشكل دوري. يُنصح بمراجعة هذه الصفحة والعودة إليها بشكل روتيني للبقاء على اطلاع بأي تعديلات قد أجريناها على شروط الخدمة. يرجى العلم أننا لن نتحمل المسؤولية إذا أصبح هذا الموقع غير متاح في أي وقت ولأي سبب من الأسباب. بالإضافة إلى ذلك، قد تكون هناك حالات عرضية حيث نقوم بتقييد الوصول إلى أقسام معينة أو الموقع بأكمله.</p>
 												
-												</p>
+												
+												
+												
+												<br><h6><strong> المحظورات</strong></h6>
+												<p>يُمنع منعًا باتًا استخدام هذا الموقع الإلكتروني بشكلٍ غير لائق؛ وهذا يعني أنه يجب عليك الامتناع عن ارتكاب الأنشطة الإجرامية أو الترويج لها، أو نقل أو نشر الفيروسات أو أي محتوى آخر ضار من منظور تكنولوجي، أو ينتهك السرية، أو مسيء أو فاحش بأي شكل من الأشكال. سيُنظر إلى أي انتهاك لهذا الشرط على أنه جريمة جنائية، وتحتفظ شركة  بالحق في الإبلاغ عن مثل هذه الانتهاكات إلى جهات إنفاذ القانون المناسبة والكشف عن هويتك لهم.</p>
+													
+												<br><h6><strong>الملكية الفكرية والبرامج والمحتوى</strong></h6>
+													<p>تظل حقوق الملكية الفكرية لجميع البرامج والمحتوى، بما في ذلك الصور الفوتوغرافية، المقدمة لك عبر هذا الموقع، ملكية حصرية لشركة ، وهي محمية بموجب قوانين حقوق الطبع والنشر والمعاهدات الدولية. يحق لك تخزين وطباعة وعرض المحتوى المقدم حصريًا للاستخدام الشخصي. ومع ذلك، يُمنع منعًا باتًا نشر أو تغيير أو توزيع أو إعادة إنتاج، بأي شكل من الأشكال، أي من المحتوى أو النسخ المكررة من المحتوى المقدم لك من خلال هذا الموقع الإلكتروني. بالإضافة إلى ذلك، لا يجوز لك استخدام هذا المحتوى في أي عمل أو نشاط تجاري.</p>
+													
+												<br><h6><strong>شروط الخدمة</strong></h6>
+													<p>نحن نعمل بالشراكة مع مدققين خارجيين مستقلين وهيئات إصدار الشهادات التي يمكنها إصدار شهادة المنظمة الدولية للمعايير (الآيزو) لمستخدمي موقع MyisoArabia عند الانتهاء بنجاح من عملية التدقيق. من المهم ملاحظة أن موقع MyisoArabia ليس لديه صلاحية إصدار الشهادات لأنه ليس هيئة اعتماد ولا هيئة لإصدار الشهادات. يكون كل ترخيص لبرنامج MyisoArabia صالحًا لمدة ثلاث سنوات، ويتوقف ذلك على إكمال ثلاث عمليات تدقيق سنوية بنجاح. إذا قمت بشراء نظامنا وتنوي الحصول على شهادة الآيزو، فيجب عليك مواصلة استخدام البرنامج وإثبات ذلك من خلال اجتياز عمليات التدقيق السنوية الثلاث. قد يؤدي عدم القيام بذلك إلى إلغاء شهادتك، وفي مثل هذه الحالات، لا يمكن أن يكون MyisoArabia™ مسؤولاً عن ذلك.</p>
+													
+												<br><h6><strong> لإرجاع</strong></h6>
+													<p>نظرًا لكوننا متخصصين في خدمات التصديق عبر الإنترنت، والتي تعد خدمة رقمية، فإننا نقوم برد الأموال خلال أول 7 أيام من شراء خدماتنا. نوصي بشدة بالاستفادة من الموارد المتاحة على موقعنا الإلكتروني، مثل الفيديو التوضيحي أو المدونات الإعلامية أو خيار المشاركة في محادثة مع أحد وكلائنا..</p>
+													<br><h6><strong> الاسترداد (إذا كان ينطبق)</strong></h6>
+													<p>نظرًا للطبيعة الرقمية لمنتجاتنا وخدماتنا، يرجى العلم أننا لا نقبل طلبات استرداد الأموال بعد انقضاء الأيام السبعة الأولى من شراء خدماتنا.</p>
+													
+												<br><h6><strong> إخلاء المسؤولية</strong></h6>
+													<p>تقدم المعلومات المعروضة على هذا الموقع الإلكتروني دون أي ضمانات أو شروط فيما يتعلق بدقتها. ما لم ينص صراحة على خلاف ذلك، وإلى الحد الأقصى الذي يسمح به القانون، فإن شركة ، جنبًا إلى جنب مع مورديها ومزودي المحتوى والجهات الإعلانية، تخلي مسؤوليتها صراحة عن جميع الشروط الضمنية والضمانات والشروط الأخرى، سواء بموجب القانون أو القانون العام أو قانون حقوق الملكية. ولن تكون مسؤولة عن أي أضرار من أي نوع، بما في ذلك على سبيل المثال لا الحصر، أي أضرار مباشرة أو غير مباشرة أو خاصة أو تبعية أو عقابية أو عرضية، أو الأضرار الناجمة عن فقدان الاستخدام أو الأرباح أو البيانات أو غيرها من الأصول غير الملموسة، أو الضرر الذي يلحق بالشهرة أو السمعة، أو تكلفة شراء السلع والخدمات البديلة، الناشئة عن أو المرتبطة باستخدام أو عدم القدرة على الاستخدام أو الأداء أو فشل هذا الموقع أو المواقع المرتبطة وأي مواد منشورة عليه، بغض النظر عما إذا كانت هذه الأضرار متوقعة أو نشأت في تكوين العقد أو نتيجة خطأ أو تقصير أو كرد حق أو التعويض بموجب القانون أو القانون العام أو غير ذلك.</p>
+													
+												<br><h6><strong> إخلاء المسؤولية فيما يخص ملكية العلامات التجارية وصور الأفراد والمحتوى المحمي بحقوق الطبع والنشر لجهة خارجية</strong></h6>
+													<p>ما لم تتم الإشارة إلى خلاف ذلك صراحةً، فإن أي أفراد (بما في ذلك أسماؤهم وصورهم) وعلامات تجارية ومحتوى وخدمات ومواقع تابعة لجهات خارجية، والمعروضة على هذا الموقع، ليس لديهم ارتباط مباشر أو اتصال أو انتماء مع شركة ، ولا ينبغي افتراض وجود مثل هذا الارتباط. كما أن العلامات التجارية والأسماء الواردة في هذا الموقع الإلكتروني هي ملك لأصحاب تلك العلامات التجارية. وعند ذكر علامة تجارية أو اسم علامة تجارية، يكون ذلك فقط لأغراض وصفية أو تعريفية فيما يتعلق بالمنتجات والخدمات، ولا يعني أي تأييد أو اتصال بشركة .</p>
+													<br><h6><strong> التعويض</strong></h6>
+													<p>تلتزم بتعويض شركة  وحمايتها وإبراء ذمتها، إلى جانب مديريها ومسؤوليها وموظفيها ومستشاريها ووكلائها والشركات التابعة لها، ضد أي مطالبات ومسؤوليات وخسائر ونفقات مرتبطة بأطراف خارجية (والتي قد تشمل النفقات القانونية) الناجمة عن استخدامك لهذا الموقع الإلكتروني أو أي انتهاكات لشروط الخدمة.</p>
+													
+												<br><h6><strong> التعديلات والتغييرات</strong></h6>
+													<p>يحق لشركة ، وفقًا لتقديرها المطلق، وفي أي وقت ودون إشعار مسبق، تعديل أو إزالة أو تغيير الخدمات وأي صفحة على هذا الموقع الإلكتروني.</p>
+													<br><h6><strong> استقلالية بنود الاتفاقية</strong></h6>
+													<p>إذا أصبح أي جزء من شروط الخدمة غير قابل للتنفيذ (بما في ذلك أي حكم يحد من مسؤوليتنا تجاهك)، فلن تتأثر صحة وقابلية تنفيذ الأجزاء المتبقية من شروط الخدمة، وستظل جميع البنود الأخرى سارية بالكامل. وبقدر الإمكان، عندما يمكن فصل أي بند/بند فرعي أو جزء من بند/بند فرعي لجعل الجزء المتبقي صالحًا، يجب تفسير البند وفقًا لذلك. وبدلاً من ذلك، فإنك توافق على تصحيح البند وتفسيره بطريقة تشبه إلى حد كبير المعنى الأصلي للبند/ البند الفرعي على النحو الذي يسمح به القانون المعمول به.</p>
+													
+												<br><h6><strong> القانون الحاكم/ الولاية القضائية</strong></h6>
+													<p>تخضع هذه الشروط والأحكام، وكذلك المعاملات الموضحة هنا، لقوانين دولة الإمارات العربية المتحدة وتفسر وفقًا لها. أي دعوى قضائية تسعى إلى الحصول على تعويضات قانونية أو عادلة تنشأ عن هذه الشروط أو ترتبط بها يجب رفعها حصريًا في المحاكم الواقعة ضمن الولاية القضائية لدولة الإمارات العربية المتحدة.</p>
+													
+												<br><h6><strong> كامل الاتفاقية</strong></h6>
+													<p>تشكل شروط الخدمة المذكورة أعلاه الاتفاقية الكاملة بين الطرفين وتلغي أي اتفاقيات سابقة أو متزامنة بينك وبين شركة . ولن يكون أي تنازل عن أي شرط في شروط الخدمة ساريًا إلا إذا كان خطيًا وموقعًا من قبل مدير شركة .</p>
+													
+												<br><h6><strong> التواصل معنا</strong></h6>
+													<p>إذا كانت لديك أسئلة أو تعليقات حول هذه الشروط والأحكام، يرجى التواصل معنا على العنوان التالي:<br>
+														شركة ج. م. إنتربرايزس ذ.م.م<br>
+														المكتب 1003، مبنى C34،<br>
+														شارع الرزقي، منطقة الدانة،<br>
+														أبوظبي، الإمارات العربية المتحدة، 22207<br>
+														البريد الإلكتروني: ceo@jmegrp.com
 													<!-- <div class="ml-2">
 															<input class="form-check-input agreeInput" type="checkbox" value="" id="TermConditions">
 															<label class="form-check-label" for="TermConditions">
@@ -230,13 +162,15 @@
 													</div> -->
 											</div>
 											<div class="modal-footer">
-												<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+												<button type="button" class="btn btn-secondary" data-dismiss="modal">يغلق</button>
 											</div>
 											</div>
 										</div>
 									</div>
         
-    </section>
+    
+	
+								</section>
     <script>
     $('#firstCheckbox').on("change",function(){
 			// $('#firstCheckbox').prop('checked', false);
