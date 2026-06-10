b01022d6ec84dbe8790aab3459a41e1e4f10c167
Abdul Basit <basitawan.abdul@gmail.com>
2026-06-10 13:36:50 +0500

    login page submit button color

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -92,7 +92,7 @@
                     </div>
 
                     <div class="text-center mb-7">
-                        <button type="submit" class="w-full inline-flex items-center justify-center px-6 py-2.5 font-bold text-base text-white rounded-md transition-all duration-500 btn-brand"  type="submit"  id="SignIN" style="background-color: #5e85c4 !important; border-color: #5e85c4 !important; opacity: 1 !important; color: #fff !important;" disabled>تسجيل الدخول</button>
+                        <button type="submit" class="w-full inline-flex items-center justify-center px-6 py-2.5 font-bold text-base text-white rounded-md transition-all duration-500 btn-brand"  type="submit"  id="SignIN" style="background-color: #5e85c4 !important; border-color: #5e85c4 !important; color: #fff !important;" disabled>تسجيل الدخول</button>
                     </div>
 
                    
