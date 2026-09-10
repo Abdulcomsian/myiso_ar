b9bf56709b1485f7776eb14986571b2e40b4574f
Abdul Basit <basitawan.abdul@gmail.com>
2026-09-10 12:48:15 +0500

    left side menu issue

--- resources/views/dashboard/includes/head.blade.php
+++ resources/views/dashboard/includes/head.blade.php
@@ -119,7 +119,6 @@ License: You must have a valid license purchased only from themeforest(the above
         <!--end::Page Vendors Styles -->
     @endif
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('css/admin-modern-layout.css') }}" rel="stylesheet" type="text/css" />
     @stack('styles')
 </head>
 <style>
